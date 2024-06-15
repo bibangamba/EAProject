80a0a1d88e8e81cbeaa999531d4b2c6c016ddd9d
Saurab Ghimire <sourrab@gmail.com>
2024-06-15 15:35:19 -0500

    working on locations sys-admin

--- src/main/java/com/cs544/project/controller/SysAdminController.java
+++ src/main/java/com/cs544/project/controller/SysAdminController.java
@@ -1,12 +1,12 @@
 package com.cs544.project.controller;
 
 import com.cs544.project.domain.Location;
+import com.cs544.project.dto.LocationDto;
+import com.cs544.project.exception.CustomNotFoundException;
 import com.cs544.project.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -19,5 +19,23 @@ public class SysAdminController {
     ResponseEntity<?> getLocations(){
         Collection<Location> locations =  locationService.getLocations();
         return ResponseEntity.ok(locations);
+    }
+
+    @PostMapping("/locations")
+    ResponseEntity<?> addLocation(@RequestBody Location location){
+        Location addedLocation =  locationService.addLocation(location);
+        return ResponseEntity.ok(addedLocation);
+    }
+
+    @PatchMapping("/locations")
+    ResponseEntity<?> updateLocation(@RequestBody LocationDto location){
+        Location addedLocation =  locationService.updateLocation(location);
+        return ResponseEntity.ok(addedLocation);
+    }
+
+    @DeleteMapping("/locations/{locationId}")
+    ResponseEntity<?> updateLocation(@PathVariable("locationId") long id) throws CustomNotFoundException {
+        locationService.deleteLocation(id);
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 }
--- src/main/java/com/cs544/project/service/LocationService.java
+++ src/main/java/com/cs544/project/service/LocationService.java
@@ -1,17 +1,33 @@
 package com.cs544.project.service;
 
 import com.cs544.project.domain.Location;
+import com.cs544.project.exception.CustomNotFoundException;
 import com.cs544.project.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class LocationService {
     @Autowired
     LocationRepository locationRepository;
+    
     public Collection<Location> getLocations(){
         return locationRepository.findAll();
+    }
+
+    public Location addLocation(Location location){
+        return locationRepository.save(location);
+    }
+
+    public Location updateLocation(Location location){
+        return locationRepository.save(location);
+    }
+
+    public void deleteLocation(long id) throws CustomNotFoundException{
+        Optional<Location> location = locationRepository.findById(id);
+        locationRepository.delete(location.orElseThrow(() -> new CustomNotFoundException("Location with id="+id+ " not found")));
     }
 }
