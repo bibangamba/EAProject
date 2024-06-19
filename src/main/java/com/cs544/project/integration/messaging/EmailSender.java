3844df467fe5e01d83524e850a3d19da2499eae3
Saurab Ghimire <sourrab@gmail.com>
2024-06-19 13:57:51 -0500

    email schedule every 4 or 8 hours before deadline

--- src/main/java/com/cs544/project/integration/messaging/EmailSender.java
+++ src/main/java/com/cs544/project/integration/messaging/EmailSender.java
@@ -1,15 +1,10 @@
 package com.cs544.project.integration.messaging;
 
-import com.cs544.project.domain.Email;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
 
 @Component
 public class EmailSender {
