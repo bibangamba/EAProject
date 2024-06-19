982e0c8dd8e1986f330b01de0f1c9bc7591ec6a1
Andrew Twijukye <andrewbibangamba@gmail.com>
2024-06-19 12:28:36 -0500

    service unit testing example

--- src/test/java/com/cs544/project/TestConfiguration.java
+++ src/test/java/com/cs544/project/TestConfiguration.java
@@ -1,6 +1,6 @@
 package com.cs544.project;
 
-import com.cs544.project.integration.messaging.Sender;
+import com.cs544.project.integration.messaging.EmailSender;
 import com.cs544.project.service.DatabaseInitService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,7 @@ import org.springframework.jms.core.JmsTemplate;
 @Configuration
 public class TestConfiguration {
     @MockBean
-    private Sender sender;
+    private EmailSender sender;
 
     @MockBean
     private JmsTemplate jmsTemplate;
