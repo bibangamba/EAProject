71adf7241d283928d8abe25bac095c4a1a0a12ff
Bisho Silwal <aayussilwal@gmail.com>
2024-06-20 11:25:36 -0500

    Use current logged in person (#32)

--- src/main/java/com/cs544/project/controller/studentView/CourseOfferingController.java
+++ src/main/java/com/cs544/project/controller/studentView/CourseOfferingController.java
@@ -1,17 +1,16 @@
 package com.cs544.project.controller.studentView;
 
 import com.cs544.project.adapter.CourseOfferingAdapter;
-import com.cs544.project.domain.AttendanceRecord;
-import com.cs544.project.domain.CourseOffering;
-import com.cs544.project.domain.Student;
+import com.cs544.project.controller.ApplicationController;
+import com.cs544.project.domain.*;
 import com.cs544.project.dto.StudentCourse;
 import com.cs544.project.exception.CustomNotFoundException;
 import com.cs544.project.repository.StudentRepository;
 import com.cs544.project.service.CourseOfferingService;
 import com.cs544.project.service.CourseRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import com.cs544.project.domain.Session;
 import com.cs544.project.service.AttendanceRecordService;
 import com.cs544.project.service.StudentService;
 
@@ -21,13 +20,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @RestController("student-view")
 @RequestMapping("/student-view/course-offerings")
-public class CourseOfferingController {
+public class CourseOfferingController extends ApplicationController {
     @Autowired
     private StudentRepository studentRepository;
 
@@ -50,8 +50,8 @@ public class CourseOfferingController {
 
     @GetMapping()
     public ResponseEntity<?> getCourseOffering() {
-        // student should came from the logged in user
-        Student student = studentRepository.findFirstByFirstName("John");
+        Person currentPerson = getCurrentPerson();
+        Student student = studentRepository.findFirstByFirstName(currentPerson.getFirstName());
         Collection<StudentCourse> studentCourses = courseRegistrationService.getCourseByStudent(student);
         if (studentCourses.isEmpty()) {
             return new ResponseEntity<String>("not found", HttpStatus.OK);
